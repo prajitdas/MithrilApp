ab2bd159bb3e8b1232da233a6bd5eda522181abd
Prajit Kumar Das <prajit1@umbc.edu>
2016-04-30 21:51:14 -0400

    basic strucutral changes

--- app/src/main/java/edu/umbc/cs/ebiquity/mithril/mithrilappmanager/ui/MainActivity.java
+++ app/src/main/java/edu/umbc/cs/ebiquity/mithril/mithrilappmanager/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package edu.umbc.cs.ebiquity.mithril.mithrilappmanager;
+package edu.umbc.cs.ebiquity.mithril.mithrilappmanager.ui;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -13,6 +13,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import edu.umbc.cs.ebiquity.mithril.mithrilappmanager.R;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -23,14 +25,14 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
