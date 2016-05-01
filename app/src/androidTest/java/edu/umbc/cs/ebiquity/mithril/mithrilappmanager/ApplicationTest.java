359098480fa2166a5d789d322bb88056112e5a68
Prajit Kumar Das <prajit1@umbc.edu>
2016-04-30 20:20:32 -0400

    init commit

---
+++ app/src/androidTest/java/edu/umbc/cs/ebiquity/mithril/mithrilappmanager/ApplicationTest.java
@@ -0,0 +1,13 @@
+package edu.umbc.cs.ebiquity.mithril.mithrilappmanager;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
