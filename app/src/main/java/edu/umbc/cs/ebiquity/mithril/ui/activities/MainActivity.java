8abc69c50448f2ef7628a94cb2e16cd3ac42eb9a
Prajit Kumar Das <prajitdas@gmail.com>
2016-10-03 00:46:07 -0400

    basic structure of app is ready

--- app/src/main/java/edu/umbc/cs/ebiquity/mithril/ui/activities/MainActivity.java
+++ app/src/main/java/edu/umbc/cs/ebiquity/mithril/ui/activities/MainActivity.java
@@ -15,7 +15,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
@@ -86,28 +85,6 @@ public class MainActivity extends AppCompatActivity
                 .commit();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -123,6 +100,12 @@ public class MainActivity extends AppCompatActivity
             loadSystemAppsFragment();
         } else if (id == R.id.nav_all) {
             loadAllAppsFragment();
+        } else if (id == R.id.nav_settings) {
+            Intent intent= new Intent(this, AppSettingsActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_about) {
+            Intent intent= new Intent(this, AboutActivity.class);
+            startActivity(intent);
         }
 
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
