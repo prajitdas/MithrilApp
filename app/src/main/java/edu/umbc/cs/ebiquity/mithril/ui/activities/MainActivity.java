38f12514518707ebd5413e59ec4abd2be842089a
Prajit Kumar Das <prajitdas@gmail.com>
2016-12-11 20:58:21 -0500

    bugs in PrefsFragment removed but need to fix up the other things

--- app/src/main/java/edu/umbc/cs/ebiquity/mithril/ui/activities/MainActivity.java
+++ app/src/main/java/edu/umbc/cs/ebiquity/mithril/ui/activities/MainActivity.java
@@ -149,6 +149,11 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
+    public void onListFragmentInteraction(PermData item) {
+        //TODO do something when the permission data is requested - I have an idea. Why don't you launch a list of permissions that are being used by apps.
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initViews();
@@ -166,7 +171,6 @@ public class MainActivity extends AppCompatActivity
 //        // Stores the connect / disconnect data in a text file
 //        editor.putString(MithrilApplication.getPrefKeyLogFilename(), logFile.getAbsolutePath());
 //        editor.commit();
-
         if (PermissionHelper.isExplicitPermissionAcquisitionNecessary()) {
             PermissionHelper.requestAllNecessaryPermissions(this);
             if (PermissionHelper.getUsageStatsPermisison(this))
@@ -225,33 +229,23 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
             case MithrilApplication.ALL_PERMISSIONS_MITHRIL_REQUEST_CODE: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
                     // permission was granted, yay! Do the
                     // contacts-related task you need to do.
-
                 } else {
-
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
                     Toast.makeText(this, "You denied some permissions. This might disrupt some functionality!", Toast.LENGTH_SHORT).show();
                 }
                 return;
             }
-
             // other 'case' lines to check for other
             // permissions this app might request
         }
-    }
-
-    @Override
-    public void onListFragmentInteraction(PermData item) {
-
     }
 }
