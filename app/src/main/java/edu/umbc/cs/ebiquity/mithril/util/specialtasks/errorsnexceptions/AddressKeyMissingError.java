1e8db16cb8c452d956e305a8486df99b3d4fe1e9
Prajit Kumar Das <prajitdas@gmail.com>
2017-01-01 06:09:08 -0500

    address key issue fixed. Places API key to be added

--- app/src/main/java/edu/umbc/cs/ebiquity/mithril/util/specialtasks/errorsnexceptions/AddressKeyMissingError.java
+++ app/src/main/java/edu/umbc/cs/ebiquity/mithril/util/specialtasks/errorsnexceptions/AddressKeyMissingError.java
@@ -4,10 +4,10 @@ package edu.umbc.cs.ebiquity.mithril.util.specialtasks.errorsnexceptions;
  * Created by Prajit on 12/26/2016.
  */
 
-public class AddressKeyMissingException extends Exception {
+public class AddressKeyMissingError extends Error {
     private String message;
 
-    public AddressKeyMissingException() {
+    public AddressKeyMissingError() {
         super();
         setMessage(new String("Please specify a key to store the address in"));
     }
