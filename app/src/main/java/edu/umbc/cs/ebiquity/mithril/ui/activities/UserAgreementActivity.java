e4043a8ad79a22a042e1b5e999c62d72bccc4f16
Prajit Kumar Das <prajitdas@gmail.com>
2017-01-18 09:37:01 -0500

    fixing splash screen

--- app/src/main/java/edu/umbc/cs/ebiquity/mithril/ui/activities/UserAgreementActivity.java
+++ app/src/main/java/edu/umbc/cs/ebiquity/mithril/ui/activities/UserAgreementActivity.java
@@ -2,13 +2,28 @@ package edu.umbc.cs.ebiquity.mithril.ui.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import edu.umbc.cs.ebiquity.mithril.R;
 
 public class UserAgreementActivity extends AppCompatActivity {
+    private Button mShowUserAgreementBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_agreement);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        setOnclickListeners();
+    }
+
+    private void setOnclickListeners() {
+
     }
 }
