c115727c0f5b5173d5a3c5dab7c4e92591c3eeb2
Prajit Kumar Das <prajitdas@gmail.com>
2017-01-19 23:25:49 -0500

    empty fragments

--- app/src/main/java/edu/umbc/cs/ebiquity/mithril/ui/fragments/EmptyFragment.java
+++ app/src/main/java/edu/umbc/cs/ebiquity/mithril/ui/fragments/EmptyFragment.java
@@ -10,44 +10,16 @@ import android.view.ViewGroup;
 
 import edu.umbc.cs.ebiquity.mithril.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link EmptyFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link EmptyFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class EmptyFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     private OnFragmentInteractionListener mListener;
 
     public EmptyFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EmptyFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EmptyFragment newInstance(String param1, String param2) {
+    public static EmptyFragment newInstance() {
         EmptyFragment fragment = new EmptyFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -55,10 +27,6 @@ public class EmptyFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -68,7 +36,6 @@ public class EmptyFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_empty, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
