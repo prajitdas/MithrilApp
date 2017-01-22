b5b38afa5c78818c93ca4414aedd8948888a0677
Prajit Kumar Das <prajitdas@gmail.com>
2017-01-21 23:24:27 -0500

    small changes to layouts

---
+++ app/src/main/java/edu/umbc/cs/ebiquity/mithril/ui/activities/RuleChangeActivity.java
@@ -0,0 +1,15 @@
+package edu.umbc.cs.ebiquity.mithril.ui.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import edu.umbc.cs.ebiquity.mithril.R;
+
+public class RuleChangeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rule_change);
+    }
+}
