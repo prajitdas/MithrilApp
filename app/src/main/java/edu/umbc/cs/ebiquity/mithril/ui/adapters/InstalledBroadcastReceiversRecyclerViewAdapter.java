4f34c4df2918b8b0cf0414dec1f9e9adaee165a6
Prajit Kumar Das <prajitdas@gmail.com>
2017-01-22 01:18:44 -0500

    adding layouts

--- app/src/main/java/edu/umbc/cs/ebiquity/mithril/ui/adapters/InstalledBroadcastReceiversRecyclerViewAdapter.java
+++ app/src/main/java/edu/umbc/cs/ebiquity/mithril/ui/adapters/InstalledBroadcastReceiversRecyclerViewAdapter.java
@@ -6,10 +6,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.List;
+
+import edu.umbc.cs.ebiquity.mithril.R;
 import edu.umbc.cs.ebiquity.mithril.ui.fragments.mainactivityfragments.BroadcastReceiversFragment.OnListFragmentInteractionListener;
 import edu.umbc.cs.ebiquity.mithril.ui.fragments.mainactivityfragments.dummy.DummyContent.DummyItem;
-
-import java.util.List;
 
 /**
  * {@link RecyclerView.Adapter} that can display a {@link DummyItem} and makes a call to the
