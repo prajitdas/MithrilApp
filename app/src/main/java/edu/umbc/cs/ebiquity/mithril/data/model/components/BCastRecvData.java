5677f9c082436ec0393d30087c5214e1d8faf02c
Prajit Kumar Das <prajitdas@gmail.com>
2017-02-05 18:15:57 -0500

    IRB approval docs

--- app/src/main/java/edu/umbc/cs/ebiquity/mithril/data/model/components/BCastRecvData.java
+++ app/src/main/java/edu/umbc/cs/ebiquity/mithril/data/model/components/BCastRecvData.java
@@ -1,15 +1,8 @@
 package edu.umbc.cs.ebiquity.mithril.data.model.components;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by Prajit on 1/25/2017.
  */
 
 public class BCastRecvData {
-    /**
-     * An array of sample items.
-     */
-    public static final List<BCastRecvData> ITEMS = new ArrayList<BCastRecvData>();
 }
