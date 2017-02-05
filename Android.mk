881ef169f81fdfd7fd33a98e4110bec047843e8c
Prajit Kumar Das <prajitdas@gmail.com>
2017-02-04 22:44:16 -0500

    make file

---
+++ Android.mk
@@ -0,0 +1,11 @@
+
+# Auto-generated module by script
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_PACKAGE_NAME := 
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_STATIC_JAVA_LIBRARIES := 
+LOCAL_CERTIFICATE := platform
+
+include $(BUILD_PACKAGE)
