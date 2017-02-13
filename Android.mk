6a9e1b4f62193f1ae35996325947c0f6ac9f2b2c
Prajit Kumar Das <prajitdas@gmail.com>
2017-02-12 19:03:41 -0500

    make files

--- Android.mk
+++ Android.mk
@@ -1,10 +1,11 @@
+
+# Auto-generated module by script
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := MithrilApp
-LOCAL_CERTIFICATE := platform
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+
 include $(BUILD_PREBUILT)
