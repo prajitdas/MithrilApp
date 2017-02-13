884917ba8b26a15553fc70439462a70040b49e8c
Prajit Kumar Das <prajitdas@gmail.com>
2017-02-12 19:28:30 -0500

    make files

--- Android.mk
+++ Android.mk
@@ -5,7 +5,9 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := MithrilApp
 LOCAL_CERTIFICATE := platform
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_PRIVILEGED_MODULE := true
+
+LOCAL_SRC_FILES := $(call all-subdir-java-files src)
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 
