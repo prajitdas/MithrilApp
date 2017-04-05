101d8941431505a1a04526c8ea31cd5269cf3740
Prajit Kumar Das <prajitdas@gmail.com>
2017-04-05 18:43:29 -0400

    Changes to be committed: 	modified:   MithrilAC.pdf 	modified:   MithrilAC.png 	modified:   MithrilAC_create.sql 	modified:   MithrilAC_drop.sql
    Signed-off-by: Prajit Kumar Das <prajitdas@gmail.com>

--- app/src/main/java/edu/umbc/ebiquity/mithril/data/dbhelpers/MithrilAC_drop.sql
+++ app/src/main/java/edu/umbc/ebiquity/mithril/data/dbhelpers/MithrilAC_drop.sql
@@ -1,15 +1,15 @@
 -- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2017-04-05 22:04:34.378
+-- Last modification date: 2017-04-05 22:42:16.807
 
 -- views
 DROP VIEW apppermview;
 
 -- foreign keys
 ALTER TABLE actionlog
-    DROP FOREIGN KEY actions_requesters;
+    DROP FOREIGN KEY actionlog_apps;
 
 ALTER TABLE actionlog
-    DROP FOREIGN KEY actions_resources;
+    DROP FOREIGN KEY actionlog_permissions;
 
 ALTER TABLE appperm
     DROP FOREIGN KEY appperm_apps;
@@ -17,29 +17,23 @@ ALTER TABLE appperm
 ALTER TABLE appperm
     DROP FOREIGN KEY appperm_permissions;
 
-ALTER TABLE apps
-    DROP FOREIGN KEY apps_requesters;
-
 ALTER TABLE contextlog
     DROP FOREIGN KEY contextlog_context;
 
-ALTER TABLE permissions
-    DROP FOREIGN KEY permissions_resources;
+ALTER TABLE policyrules
+    DROP FOREIGN KEY policyrules_apps;
 
 ALTER TABLE policyrules
     DROP FOREIGN KEY policyrules_context;
 
 ALTER TABLE policyrules
-    DROP FOREIGN KEY policyrules_requesters;
-
-ALTER TABLE policyrules
-    DROP FOREIGN KEY policyrules_resources;
+    DROP FOREIGN KEY policyrules_permissions;
 
 ALTER TABLE violationlog
-    DROP FOREIGN KEY violations_requesters;
+    DROP FOREIGN KEY violationlog_apps;
 
 ALTER TABLE violationlog
-    DROP FOREIGN KEY violations_resources;
+    DROP FOREIGN KEY violationlog_permissions;
 
 -- tables
 DROP TABLE actionlog;
@@ -55,10 +49,6 @@ DROP TABLE contextlog;
 DROP TABLE permissions;
 
 DROP TABLE policyrules;
-
-DROP TABLE requesters;
-
-DROP TABLE resources;
 
 DROP TABLE violationlog;
 
