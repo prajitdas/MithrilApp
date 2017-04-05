2a655d58c29f899981351646cd126a72bdf83790
Prajit Kumar Das <prajitdas@gmail.com>
2017-04-05 18:43:29 -0400

    Changes to be committed: 	modified:   MithrilAC.pdf 	modified:   MithrilAC.png 	modified:   MithrilAC_create.sql 	modified:   MithrilAC_drop.sql

--- app/src/main/java/edu/umbc/ebiquity/mithril/data/dbhelpers/MithrilAC_create.sql
+++ app/src/main/java/edu/umbc/ebiquity/mithril/data/dbhelpers/MithrilAC_create.sql
@@ -1,14 +1,14 @@
 -- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2017-04-05 22:04:34.378
+-- Last modification date: 2017-04-05 22:42:16.807
 
 -- tables
 -- Table: actionlog
 CREATE TABLE actionlog (
     id int NOT NULL AUTO_INCREMENT,
-    resources_id int NOT NULL,
-    requesters_id int NOT NULL,
     time timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
     action int NOT NULL,
+    apps_id int NOT NULL,
+    permissions_id int NOT NULL,
     CONSTRAINT actionlog_pk PRIMARY KEY (id)
 ) COMMENT 'Table showing actions taken for each context, resource, requester tuple';
 
@@ -34,7 +34,6 @@ CREATE TABLE apps (
     installed bool NOT NULL DEFAULT true,
     type int NOT NULL,
     installdate timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
-    requesters_id int NOT NULL,
     UNIQUE INDEX apps_unique_key (name),
     CONSTRAINT apps_pk PRIMARY KEY (id)
 ) COMMENT 'Table showing metadata for apps';
@@ -71,7 +70,6 @@ CREATE TABLE permissions (
     flag text NULL,
     description text NULL,
     icon blob NOT NULL,
-    resources_id int NOT NULL,
     UNIQUE INDEX permissions_unique_name (name),
     CONSTRAINT permissions_pk PRIMARY KEY (id)
 ) COMMENT 'Table showing metadata for permissions';
@@ -81,34 +79,20 @@ CREATE TABLE policyrules (
     id int NOT NULL AUTO_INCREMENT,
     name text NOT NULL,
     action int NOT NULL,
-    requesters_id int NOT NULL,
-    resources_id int NOT NULL,
     context_id int NOT NULL,
+    apps_id int NOT NULL,
+    permissions_id int NOT NULL,
     CONSTRAINT policyrules_pk PRIMARY KEY (id)
 ) COMMENT 'Table showing policy rules defined for apps and requested resources in given context';
-
--- Table: requesters
-CREATE TABLE requesters (
-    id int NOT NULL AUTO_INCREMENT,
-    name text NOT NULL DEFAULT *,
-    CONSTRAINT requester_id PRIMARY KEY (id)
-) COMMENT 'Table showing metadata for requesters of user data';
-
--- Table: resources
-CREATE TABLE resources (
-    id int NOT NULL AUTO_INCREMENT,
-    name text NOT NULL,
-    CONSTRAINT resource_id PRIMARY KEY (id)
-) COMMENT 'Table showing metadata for resource being requested';
 
 -- Table: violationlog
 CREATE TABLE violationlog (
     id int NOT NULL AUTO_INCREMENT,
-    resources_id int NOT NULL,
-    requesters_id int NOT NULL,
     description text NOT NULL,
     marker bool NULL,
     time timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+    apps_id int NOT NULL,
+    permissions_id int NOT NULL,
     CONSTRAINT violationlog_pk PRIMARY KEY (id)
 ) COMMENT 'Table showing violations recorded by MithrilAC and subsequent user feedback';
 
@@ -131,13 +115,13 @@ AND
 ap.permissions_id = p.id;
 
 -- foreign keys
--- Reference: actions_requesters (table: actionlog)
-ALTER TABLE actionlog ADD CONSTRAINT actions_requesters FOREIGN KEY actions_requesters (requesters_id)
-    REFERENCES requesters (id);
+-- Reference: actionlog_apps (table: actionlog)
+ALTER TABLE actionlog ADD CONSTRAINT actionlog_apps FOREIGN KEY actionlog_apps (apps_id)
+    REFERENCES apps (id);
 
--- Reference: actions_resources (table: actionlog)
-ALTER TABLE actionlog ADD CONSTRAINT actions_resources FOREIGN KEY actions_resources (resources_id)
-    REFERENCES resources (id);
+-- Reference: actionlog_permissions (table: actionlog)
+ALTER TABLE actionlog ADD CONSTRAINT actionlog_permissions FOREIGN KEY actionlog_permissions (permissions_id)
+    REFERENCES permissions (id);
 
 -- Reference: appperm_apps (table: appperm)
 ALTER TABLE appperm ADD CONSTRAINT appperm_apps FOREIGN KEY appperm_apps (apps_id)
@@ -148,37 +132,29 @@ ALTER TABLE appperm ADD CONSTRAINT appperm_apps FOREIGN KEY appperm_apps (apps_i
 ALTER TABLE appperm ADD CONSTRAINT appperm_permissions FOREIGN KEY appperm_permissions (permissions_id)
     REFERENCES permissions (id);
 
--- Reference: apps_requesters (table: apps)
-ALTER TABLE apps ADD CONSTRAINT apps_requesters FOREIGN KEY apps_requesters (requesters_id)
-    REFERENCES requesters (id);
-
 -- Reference: contextlog_context (table: contextlog)
 ALTER TABLE contextlog ADD CONSTRAINT contextlog_context FOREIGN KEY contextlog_context (instance_id)
     REFERENCES context (id);
 
--- Reference: permissions_resources (table: permissions)
-ALTER TABLE permissions ADD CONSTRAINT permissions_resources FOREIGN KEY permissions_resources (resources_id)
-    REFERENCES resources (id);
+-- Reference: policyrules_apps (table: policyrules)
+ALTER TABLE policyrules ADD CONSTRAINT policyrules_apps FOREIGN KEY policyrules_apps (apps_id)
+    REFERENCES apps (id);
 
 -- Reference: policyrules_context (table: policyrules)
 ALTER TABLE policyrules ADD CONSTRAINT policyrules_context FOREIGN KEY policyrules_context (context_id)
     REFERENCES context (id);
 
--- Reference: policyrules_requesters (table: policyrules)
-ALTER TABLE policyrules ADD CONSTRAINT policyrules_requesters FOREIGN KEY policyrules_requesters (requesters_id)
-    REFERENCES requesters (id);
+-- Reference: policyrules_permissions (table: policyrules)
+ALTER TABLE policyrules ADD CONSTRAINT policyrules_permissions FOREIGN KEY policyrules_permissions (permissions_id)
+    REFERENCES permissions (id);
 
--- Reference: policyrules_resources (table: policyrules)
-ALTER TABLE policyrules ADD CONSTRAINT policyrules_resources FOREIGN KEY policyrules_resources (resources_id)
-    REFERENCES resources (id);
+-- Reference: violationlog_apps (table: violationlog)
+ALTER TABLE violationlog ADD CONSTRAINT violationlog_apps FOREIGN KEY violationlog_apps (apps_id)
+    REFERENCES apps (id);
 
--- Reference: violations_requesters (table: violationlog)
-ALTER TABLE violationlog ADD CONSTRAINT violations_requesters FOREIGN KEY violations_requesters (requesters_id)
-    REFERENCES requesters (id);
-
--- Reference: violations_resources (table: violationlog)
-ALTER TABLE violationlog ADD CONSTRAINT violations_resources FOREIGN KEY violations_resources (resources_id)
-    REFERENCES resources (id);
+-- Reference: violationlog_permissions (table: violationlog)
+ALTER TABLE violationlog ADD CONSTRAINT violationlog_permissions FOREIGN KEY violationlog_permissions (permissions_id)
+    REFERENCES permissions (id);
 
 -- End of file.
 
