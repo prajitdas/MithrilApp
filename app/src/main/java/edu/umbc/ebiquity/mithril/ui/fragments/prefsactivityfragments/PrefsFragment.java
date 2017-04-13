c6f09e98de8b5aa52d2eaecebd84febd0b63f3e1
Prajit Kumar Das <prajitdas@gmail.com>
2017-04-13 15:53:23 -0400

    refactor
    Signed-off-by: Prajit Kumar Das <prajitdas@gmail.com>

--- app/src/main/java/edu/umbc/ebiquity/mithril/ui/fragments/prefsactivityfragments/PrefsFragment.java
+++ app/src/main/java/edu/umbc/ebiquity/mithril/ui/fragments/prefsactivityfragments/PrefsFragment.java
@@ -1,7 +1,6 @@
 package edu.umbc.ebiquity.mithril.ui.fragments.prefsactivityfragments;
 
 import android.app.Activity;
-import android.app.AppOpsManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -30,9 +29,6 @@ import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 import edu.umbc.ebiquity.mithril.MithrilApplication;
 import edu.umbc.ebiquity.mithril.R;
@@ -104,48 +100,6 @@ public class PrefsFragment extends PreferenceFragment {
                 websiteUri));
     }
 
-    /**
-     * See this: https://forums.xamarin.com/discussion/64456/c-java-reflection-access-private-methods-on-underlying-native-instance
-     *
-     * @param context
-     * @param code
-     * @param uid
-     * @param mode
-     * @return
-     */
-    private static boolean setMode(Context context, int code,
-                                   int uid, int mode) {
-        AppOpsManager appOpsManager =
-                (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
-        Class appOpsManagerClass = appOpsManager.getClass();
-
-        try {
-            Class[] types = new Class[4];
-            types[0] = Integer.TYPE;
-            types[1] = Integer.TYPE;
-            types[2] = String.class;
-            types[3] = Integer.TYPE;
-            Method setModeMethod =
-                    appOpsManagerClass.getMethod("setMode", types);
-
-            Object[] args = new Object[4];
-            args[0] = Integer.valueOf(code);
-            args[1] = Integer.valueOf(uid);
-            args[2] = context.getPackageName();
-            args[3] = Integer.valueOf(mode);
-            setModeMethod.invoke(appOpsManager, args);
-
-            return true;
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -153,7 +107,6 @@ public class PrefsFragment extends PreferenceFragment {
         sharedPrefs = context.getSharedPreferences(MithrilApplication.getSharedPreferencesName(), MODE_PRIVATE);
         editor = sharedPrefs.edit();
 
-//        appOps();
         initViews();
         initData();
         setOnPreferenceClickListeners();
@@ -250,46 +203,6 @@ public class PrefsFragment extends PreferenceFragment {
         editor.putString(MithrilApplication.getPrefPresenceInfoColleagueKey(), stringValue);
 
         editor.apply();
-    }
-
-    private void appOps() {
-//        PackageInfo mPackageInfo = null;
-//        String mPackageName = null;
-//        boolean newState = false;
-        AppOpsManager mAppOps = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
-//        mAppOps.setPrivacyGuardSettingForPackage(app.uid, app.packageName, app.privacyGuardEnabled);
-       /*
-        mAppOps.setMode(AppOpsManager.OP_WRITE_SETTINGS,
-                mPackageInfo.applicationInfo.uid, mPackageName, newState
-                        ? AppOpsManager.MODE_ALLOWED : AppOpsManager.MODE_ERRORED);
-        mCurSysAppOpMode = mAppOps.checkOp(AppOpsManager.OP_SYSTEM_ALERT_WINDOW, uid, pkg);
-        mCurToastAppOpMode = mAppOps.checkOp(AppOpsManager.OP_TOAST_WINDOW, uid, pkg);
-        mAppOps.setMode(AppOpsManager.OP_SYSTEM_ALERT_WINDOW, uid, pkg, AppOpsManager.MODE_IGNORED);
-        mAppOps.setMode(AppOpsManager.OP_TOAST_WINDOW, uid, pkg, AppOpsManager.MODE_IGNORED);
-        mAppOps.setMode(AppOpsManager.OP_SYSTEM_ALERT_WINDOW, uid, pkg, mCurSysAppOpMode);
-        mAppOps.setMode(AppOpsManager.OP_TOAST_WINDOW, uid, pkg, mCurToastAppOpMode);
-        final int switchOp = AppOpsManager.opToSwitch(firstOp.getOp());
-        int mode = mAppOps.checkOp(switchOp, entry.getPackageOps().getUid(), entry.getPackageOps().getPackageName());
-        mAppOps.setMode(switchOp, entry.getPackageOps().getUid(), entry.getPackageOps().getPackageName(), positionToMode(position));
-        sw.setChecked(mAppOps.checkOp(switchOp, entry.getPackageOps()
-                .getUid(), entry.getPackageOps().getPackageName()) == AppOpsManager.MODE_ALLOWED);
-        sw.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView,
-                                         boolean isChecked) {
-                mAppOps.setMode(switchOp, entry.getPackageOps()
-                                .getUid(), entry.getPackageOps()
-                                .getPackageName(),
-                        isChecked ? AppOpsManager.MODE_ALLOWED
-                                : AppOpsManager.MODE_IGNORED);
-            }
-        });
-        List<AppOpsManager.PackageOps> pkgs;
-        if (packageName != null) {
-            pkgs = mAppOps.getOpsForPackage(uid, packageName, tpl.ops);
-        } else {
-            pkgs = mAppOps.getPackagesForOps(tpl.ops);
-        }
-         */
     }
 
     private void setEnabledEditTexts() {
---
+++ app/src/main/java/edu/umbc/ebiquity/mithril/util/specialtasks/execute/AppOps.java
@@ -0,0 +1,102 @@
+package edu.umbc.ebiquity.mithril.util.specialtasks.execute;
+
+import android.app.AppOpsManager;
+import android.content.Context;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * Created by Prajit on 4/13/2017.
+ */
+
+public class AppOps {
+
+    private AppOpsManager appOpsManager;
+
+    public AppOps(AppOpsManager appOpsManager) {
+        this.appOpsManager = appOpsManager;
+    }
+
+    /**
+     * See this: https://forums.xamarin.com/discussion/64456/c-java-reflection-access-private-methods-on-underlying-native-instance
+     *
+     * @param context
+     * @param code
+     * @param uid
+     * @param mode
+     * @return
+     */
+    private boolean setMode(Context context, int code,
+                            int uid, int mode) {
+        AppOpsManager appOpsManager =
+                (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
+        Class appOpsManagerClass = appOpsManager.getClass();
+
+        try {
+            Class[] types = new Class[4];
+            types[0] = Integer.TYPE;
+            types[1] = Integer.TYPE;
+            types[2] = String.class;
+            types[3] = Integer.TYPE;
+            Method setModeMethod =
+                    appOpsManagerClass.getMethod("setMode", types);
+
+            Object[] args = new Object[4];
+            args[0] = Integer.valueOf(code);
+            args[1] = Integer.valueOf(uid);
+            args[2] = context.getPackageName();
+            args[3] = Integer.valueOf(mode);
+            setModeMethod.invoke(appOpsManager, args);
+
+            return true;
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private void appOps() {
+//        PackageInfo mPackageInfo = null;
+//        String mPackageName = null;
+//        boolean newState = false;
+//        AppOpsManager mAppOps = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
+//        mAppOps.setPrivacyGuardSettingForPackage(app.uid, app.packageName, app.privacyGuardEnabled);
+/*
+        mAppOps.setMode(AppOpsManager.OP_WRITE_SETTINGS,
+                mPackageInfo.applicationInfo.uid, mPackageName, newState
+                        ? AppOpsManager.MODE_ALLOWED : AppOpsManager.MODE_ERRORED);
+        mCurSysAppOpMode = mAppOps.checkOp(AppOpsManager.OP_SYSTEM_ALERT_WINDOW, uid, pkg);
+        mCurToastAppOpMode = mAppOps.checkOp(AppOpsManager.OP_TOAST_WINDOW, uid, pkg);
+        mAppOps.setMode(AppOpsManager.OP_SYSTEM_ALERT_WINDOW, uid, pkg, AppOpsManager.MODE_IGNORED);
+        mAppOps.setMode(AppOpsManager.OP_TOAST_WINDOW, uid, pkg, AppOpsManager.MODE_IGNORED);
+        mAppOps.setMode(AppOpsManager.OP_SYSTEM_ALERT_WINDOW, uid, pkg, mCurSysAppOpMode);
+        mAppOps.setMode(AppOpsManager.OP_TOAST_WINDOW, uid, pkg, mCurToastAppOpMode);
+        final int switchOp = AppOpsManager.opToSwitch(firstOp.getOp());
+        int mode = mAppOps.checkOp(switchOp, entry.getPackageOps().getUid(), entry.getPackageOps().getPackageName());
+        mAppOps.setMode(switchOp, entry.getPackageOps().getUid(), entry.getPackageOps().getPackageName(), positionToMode(position));
+        sw.setChecked(mAppOps.checkOp(switchOp, entry.getPackageOps()
+                .getUid(), entry.getPackageOps().getPackageName()) == AppOpsManager.MODE_ALLOWED);
+        sw.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView,
+                                         boolean isChecked) {
+                mAppOps.setMode(switchOp, entry.getPackageOps()
+                                .getUid(), entry.getPackageOps()
+                                .getPackageName(),
+                        isChecked ? AppOpsManager.MODE_ALLOWED
+                                : AppOpsManager.MODE_IGNORED);
+            }
+        });
+        List<AppOpsManager.PackageOps> pkgs;
+        if (packageName != null) {
+            pkgs = mAppOps.getOpsForPackage(uid, packageName, tpl.ops);
+        } else {
+            pkgs = mAppOps.getPackagesForOps(tpl.ops);
+        }
+*/
+    }
+}
