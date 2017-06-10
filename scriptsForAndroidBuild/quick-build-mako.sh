c868ea3c79b08050356e084710259a63a6640377
Prajit Kumar Das <prajitdas@gmail.com>
2017-06-10 18:38:54 -0400

    data

--- scriptsForAndroidBuild/quick-build-mako.sh
+++ scriptsForAndroidBuild/quick-build-mako.sh
@@ -14,8 +14,6 @@ if [[ $model == $expected ]]; then
         breakfast $build
         cd ~/android/$build-lineage-14.1-experimental-prajit/device/lge/$build
         ./extract-files.sh
-#	cp "/home/prajit/android/proprietary_vendor_stuff/proprietary_vendor_lge/bullhead/proprietary/app/Tycho/Tycho.apk" "/home/prajit/android/bullhead-lineage-14.1-experimental-prajit/vendor/lge/bullhead/proprietary/app/Tycho/"
-#	cp "/home/prajit/android/proprietary_vendor_stuff/proprietary_vendor_lge/bullhead/proprietary/priv-app/GCS/GCS.apk" "/home/prajit/android/bullhead-lineage-14.1-experimental-prajit/vendor/lge/bullhead/proprietary/priv-app/GCS/"
         export USE_CCACHE=1
         croot
         prebuilts/misc/linux-x86/ccache/ccache -M 50G
