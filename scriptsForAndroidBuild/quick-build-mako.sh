a91c9dace2795ff093982249eafb41963a55e5d5
Prajit Kumar Das <prajitdas@gmail.com>
2017-06-10 14:47:50 -0400

    data

---
+++ scriptsForAndroidBuild/quick-build-mako.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Sample quick build for bullhead
+device=`adb devices | awk '{if($2 == "device") print $1;}' | tr -d '\n'`
+model=`adb -s $device shell getprop ro.product.model | tr -d '\n'`
+date=`date +%Y%m%d | tr -d '\n'`
+build="mako"
+expected="Nexus 4"
+filename="lineage-14.1-"$date"-UNOFFICIAL-"$build".zip"
+if [[ $model == $expected ]]; then
+        #Measure execution time
+        start_time=`date +%s`
+        make clobber
+        source build/envsetup.sh
+        breakfast $build
+        cd ~/android/$build-lineage-14.1-experimental-prajit/device/lge/$build
+        ./extract-files.sh
+#	cp "/home/prajit/android/proprietary_vendor_stuff/proprietary_vendor_lge/bullhead/proprietary/app/Tycho/Tycho.apk" "/home/prajit/android/bullhead-lineage-14.1-experimental-prajit/vendor/lge/bullhead/proprietary/app/Tycho/"
+#	cp "/home/prajit/android/proprietary_vendor_stuff/proprietary_vendor_lge/bullhead/proprietary/priv-app/GCS/GCS.apk" "/home/prajit/android/bullhead-lineage-14.1-experimental-prajit/vendor/lge/bullhead/proprietary/priv-app/GCS/"
+        export USE_CCACHE=1
+        croot
+        prebuilts/misc/linux-x86/ccache/ccache -M 50G
+        export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4G"
+        brunch $build
+        cd $OUT
+	if [[ -f "$filename" ]]; then
+		adb push $filename /sdcard/
+		adb reboot recovery
+	fi
+        end_time=`date +%s`
+        echo execution time was `expr $end_time - $start_time` s.
+else
+        echo "You have your $model device connected to the computer. This is a build process for the $expected. Please connect your $expected device to the computer and restart the build."
+fi
