3189204cf8d11173e2e3d99d62e9a054c0f7db2d
Prajit Kumar Das <prajitdas@gmail.com>
2017-06-14 10:02:16 -0400

    app changes

--- app/src/main/java/edu/umbc/ebiquity/mithril/ui/activities/InstanceCreationActivity.java
+++ app/src/main/java/edu/umbc/ebiquity/mithril/ui/activities/InstanceCreationActivity.java
@@ -857,6 +857,123 @@ public class InstanceCreationActivity extends AppCompatActivity
         Log.i(MithrilApplication.getDebugTag(), "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
     }
 
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // The connection to Google Play services was lost for some reason.
+        Log.i(MithrilApplication.getDebugTag(), "Connection suspended");
+
+        // onConnected() will be called again automatically when the service reconnects
+    }
+
+    private void addContextToDB(String type, String label) {
+        MithrilDBHelper.getHelper(this).addContext(mithrilDB, type, label);
+    }
+
+    private void removeContextFromDB(String label) {
+        MithrilDBHelper.getHelper(this).deleteContext(mithrilDB, label);
+    }
+
+    private void enableContext(String contextType, String contextLabel, String serializedJsonContext) {
+        editor.putString(contextType + contextLabel, serializedJsonContext);
+        editor.apply();
+    }
+
+    private void disableContext(String contextType, String contextLabel) {
+        if (sharedPreferences.contains(contextType + contextLabel)) {
+            editor.remove(contextType + contextLabel);
+            editor.apply();
+        }
+    }
+
+    class AddressResultReceiver extends ResultReceiver {
+        private Context context;
+        private SharedPreferences sharedPref;
+        /**
+         * The formatted location address.
+         */
+        private Address mAddressOutput;
+        /**
+         * Tracks whether the user has requested an address. Becomes true when the user requests an
+         * address and false when the address (or an error message) is delivered.
+         * The user requests an address by pressing the Fetch Address button. This may happen
+         * before GoogleApiClient connects. This activity uses this boolean to keep track of the
+         * user's intent. If the value is true, the activity tries to fetch the address as soon as
+         * GoogleApiClient connects.
+         */
+        private boolean mAddressRequested;
+
+        public AddressResultReceiver(Handler handler, Context aContext) {
+            super(handler);
+
+            context = aContext;
+            // Set defaults, then update using values stored in the Bundle.
+            mAddressRequested = false;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+                mAddressOutput = new Address(context.getResources().getConfiguration().getLocales().get(0));
+            else
+                mAddressOutput = new Address(context.getResources().getConfiguration().locale);
+            sharedPref = context.getSharedPreferences(MithrilApplication.getSharedPreferencesName(), Context.MODE_PRIVATE);
+        }
+
+        @Override
+        protected void onReceiveResult(int resultCode, Bundle resultData) {
+            mAddressRequested = resultData.getBoolean(MithrilApplication.ADDRESS_REQUESTED_EXTRA, false);
+            String key = resultData.getString(MithrilApplication.ADDRESS_KEY, null);
+            if (key.equals(null))
+                throw new AddressKeyMissingError();
+            else
+                storeAddressInDB(key, resultCode, resultData);
+        }
+
+        protected void storeAddressInDB(String key, int resultCode, Bundle resultData) {
+            // Display the address string
+            // or an error message sent from the intent service.
+            Gson gson = new Gson();
+            String json = resultData.getString(MithrilApplication.RESULT_DATA_KEY, "");
+            try {
+                mAddressOutput = gson.fromJson(json, Address.class);
+            } catch (JsonSyntaxException e) {
+                Log.d(MithrilApplication.getDebugTag(), e.getMessage());
+            }
+
+            Log.d(MithrilApplication.getDebugTag(), "Prefs address " + resultData.getString(MithrilApplication.ADDRESS_KEY) + mAddressRequested + key + json);
+            // Show a toast message if an address was found.
+            if (resultCode == MithrilApplication.SUCCESS_RESULT) {
+//                Log.d(MithrilApplication.getDebugMithrilApplication.getDebugTag()(), getString(R.string.address_found) + ":" + mAddressOutput);
+//                PermissionHelper.toast(context, getString(R.string.address_found) + ":" + mAddressOutput);
+                addContextToDB(MithrilApplication.getPrefKeyContextTypeLocation(), key);
+                storeInSharedPreferences(key, mAddressOutput);
+            }
+            // Reset. Enable the Fetch Address button and stop showing the progress bar.
+            mAddressRequested = false;
+        }
+
+        /**
+         * From http://stackoverflow.com/a/18463758/1816861
+         *
+         * @param key
+         * @param address To Retreive
+         *                Gson gson = new Gson();
+         *                String json = mPrefs.getString("MyObject", "");
+         *                MyObject obj = gson.fromJson(json, MyObject.class);
+         */
+        public void storeInSharedPreferences(String key, Address address) {
+            SemanticLocation tempSemanticLocation = null;
+            for (Map.Entry<String, SemanticLocation> semanticLocation : semanticLocations.entrySet())
+                if (semanticLocation.getKey().equals(key))
+                    tempSemanticLocation = semanticLocation.getValue();
+            tempSemanticLocation.setAddress(address);
+            semanticLocations.put(key, tempSemanticLocation);
+
+            enableContext(MithrilApplication.getPrefKeyContextTypeLocation(),
+                    key,
+                    InstanceCreationActivity.contextDataStoreGson.toJson(tempSemanticLocation));
+
+            //Locations have been added; update view
+            loadSemanticLocationFragment();
+        }
+    }
+}
 //    /**
 //     * Builds and returns a GeofencingRequest. Specifies the list of geofences to be monitored.
 //     * Also specifies how the geofence notifications are initially triggered.
@@ -1037,121 +1154,3 @@ public class InstanceCreationActivity extends AppCompatActivity
 //                .build());
 ////        }
 //    }
-
-    @Override
-    public void onConnectionSuspended(int cause) {
-        // The connection to Google Play services was lost for some reason.
-        Log.i(MithrilApplication.getDebugTag(), "Connection suspended");
-
-        // onConnected() will be called again automatically when the service reconnects
-    }
-
-    private void addContextToDB(String type, String label) {
-        MithrilDBHelper.getHelper(this).addContext(mithrilDB, type, label);
-    }
-
-    private void removeContextFromDB(String label) {
-        MithrilDBHelper.getHelper(this).deleteContext(mithrilDB, label);
-    }
-
-    private void enableContext(String contextType, String contextLabel, String serializedJsonContext) {
-        editor.putString(contextType + contextLabel, serializedJsonContext);
-        editor.apply();
-    }
-
-    private void disableContext(String contextType, String contextLabel) {
-        if (sharedPreferences.contains(contextType + contextLabel)) {
-            editor.remove(contextType + contextLabel);
-            editor.apply();
-        }
-    }
-
-    class AddressResultReceiver extends ResultReceiver {
-        private Context context;
-        private SharedPreferences sharedPref;
-        /**
-         * The formatted location address.
-         */
-        private Address mAddressOutput;
-        /**
-         * Tracks whether the user has requested an address. Becomes true when the user requests an
-         * address and false when the address (or an error message) is delivered.
-         * The user requests an address by pressing the Fetch Address button. This may happen
-         * before GoogleApiClient connects. This activity uses this boolean to keep track of the
-         * user's intent. If the value is true, the activity tries to fetch the address as soon as
-         * GoogleApiClient connects.
-         */
-        private boolean mAddressRequested;
-
-        public AddressResultReceiver(Handler handler, Context aContext) {
-            super(handler);
-
-            context = aContext;
-            // Set defaults, then update using values stored in the Bundle.
-            mAddressRequested = false;
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
-                mAddressOutput = new Address(context.getResources().getConfiguration().getLocales().get(0));
-            else
-                mAddressOutput = new Address(context.getResources().getConfiguration().locale);
-            sharedPref = context.getSharedPreferences(MithrilApplication.getSharedPreferencesName(), Context.MODE_PRIVATE);
-        }
-
-        @Override
-        protected void onReceiveResult(int resultCode, Bundle resultData) {
-            mAddressRequested = resultData.getBoolean(MithrilApplication.ADDRESS_REQUESTED_EXTRA, false);
-            String key = resultData.getString(MithrilApplication.ADDRESS_KEY, null);
-            if (key.equals(null))
-                throw new AddressKeyMissingError();
-            else
-                storeAddressInDB(key, resultCode, resultData);
-        }
-
-        protected void storeAddressInDB(String key, int resultCode, Bundle resultData) {
-            // Display the address string
-            // or an error message sent from the intent service.
-            Gson gson = new Gson();
-            String json = resultData.getString(MithrilApplication.RESULT_DATA_KEY, "");
-            try {
-                mAddressOutput = gson.fromJson(json, Address.class);
-            } catch (JsonSyntaxException e) {
-                Log.d(MithrilApplication.getDebugTag(), e.getMessage());
-            }
-
-            Log.d(MithrilApplication.getDebugTag(), "Prefs address " + resultData.getString(MithrilApplication.ADDRESS_KEY) + mAddressRequested + key + json);
-            // Show a toast message if an address was found.
-            if (resultCode == MithrilApplication.SUCCESS_RESULT) {
-//                Log.d(MithrilApplication.getDebugMithrilApplication.getDebugTag()(), getString(R.string.address_found) + ":" + mAddressOutput);
-//                PermissionHelper.toast(context, getString(R.string.address_found) + ":" + mAddressOutput);
-                addContextToDB(MithrilApplication.getPrefKeyContextTypeLocation(), key);
-                storeInSharedPreferences(key, mAddressOutput);
-            }
-            // Reset. Enable the Fetch Address button and stop showing the progress bar.
-            mAddressRequested = false;
-        }
-
-        /**
-         * From http://stackoverflow.com/a/18463758/1816861
-         *
-         * @param key
-         * @param address To Retreive
-         *                Gson gson = new Gson();
-         *                String json = mPrefs.getString("MyObject", "");
-         *                MyObject obj = gson.fromJson(json, MyObject.class);
-         */
-        public void storeInSharedPreferences(String key, Address address) {
-            SemanticLocation tempSemanticLocation = null;
-            for (Map.Entry<String, SemanticLocation> semanticLocation : semanticLocations.entrySet())
-                if (semanticLocation.getKey().equals(key))
-                    tempSemanticLocation = semanticLocation.getValue();
-            tempSemanticLocation.setAddress(address);
-            semanticLocations.put(key, tempSemanticLocation);
-
-            enableContext(MithrilApplication.getPrefKeyContextTypeLocation(),
-                    key,
-                    InstanceCreationActivity.contextDataStoreGson.toJson(tempSemanticLocation));
-
-            //Locations have been added; update view
-            loadSemanticLocationFragment();
-        }
-    }
-}
