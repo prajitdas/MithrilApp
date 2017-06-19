2fbf9c6afbba8e10d0d73222760aeba5bdef8d77
Prajit Kumar Das <prajitdas@gmail.com>
2017-06-18 21:31:32 -0400

    app changes

--- app/src/main/java/edu/umbc/ebiquity/mithril/simulations/DataGenerator.java
+++ app/src/main/java/edu/umbc/ebiquity/mithril/simulations/DataGenerator.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.app.AppOpsManager;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 import edu.umbc.ebiquity.mithril.MithrilAC;
 import edu.umbc.ebiquity.mithril.data.dbhelpers.MithrilDBHelper;
@@ -77,18 +78,9 @@ public class DataGenerator {
                         actionAllow, mithrilDB, context));
     }
 
-    public static void setPolicyChatAppsWriteSmsAccessAtWork(SQLiteDatabase mithrilDB, Context context)
-            throws SemanticInconsistencyException {
-        MithrilDBHelper.getHelper(context).addPolicyRule(mithrilDB, DataGenerator.createPolicyRule(5,
-                        "com.google.android.talk", "Hangouts",
-                        Manifest.permission.WRITE_SMS,
-                        MithrilAC.getPrefWorkLocationKey(), MithrilAC.getPrefKeyContextTypeLocation(),
-                        actionAllow, mithrilDB, context));
-    }
-
     public static void setPolicyChatAppsReceiveSmsAccessAtWork(SQLiteDatabase mithrilDB, Context context)
             throws SemanticInconsistencyException {
-        MithrilDBHelper.getHelper(context).addPolicyRule(mithrilDB, DataGenerator.createPolicyRule(6,
+        MithrilDBHelper.getHelper(context).addPolicyRule(mithrilDB, DataGenerator.createPolicyRule(5,
                         "com.google.android.talk", "Hangouts",
                         Manifest.permission.RECEIVE_SMS,
                         MithrilAC.getPrefWorkLocationKey(), MithrilAC.getPrefKeyContextTypeLocation(),
@@ -97,7 +89,7 @@ public class DataGenerator {
 
     public static void setPolicyChatAppsSendSmsAccessAtWork(SQLiteDatabase mithrilDB, Context context)
             throws SemanticInconsistencyException {
-        MithrilDBHelper.getHelper(context).addPolicyRule(mithrilDB, DataGenerator.createPolicyRule(7,
+        MithrilDBHelper.getHelper(context).addPolicyRule(mithrilDB, DataGenerator.createPolicyRule(6,
                         "com.google.android.talk", "Hangouts",
                         Manifest.permission.SEND_SMS,
                         MithrilAC.getPrefWorkLocationKey(), MithrilAC.getPrefKeyContextTypeLocation(),
@@ -107,12 +99,12 @@ public class DataGenerator {
     /************ Email clients to be allowed calendar access at work if it's a weekday ************/
     public static void setPolicyEmailClientsReadCalendarAccessAtWorkDuringWeekdays(SQLiteDatabase mithrilDB, Context context)
             throws SemanticInconsistencyException {
-        MithrilDBHelper.getHelper(context).addPolicyRule(mithrilDB, DataGenerator.createPolicyRule(8,
+        MithrilDBHelper.getHelper(context).addPolicyRule(mithrilDB, DataGenerator.createPolicyRule(7,
                         "com.google.android.gm", "Gmail",
                         Manifest.permission.READ_CALENDAR,
                         MithrilAC.getPrefWorkLocationKey(), MithrilAC.getPrefKeyContextTypeLocation(),
                         actionAllow, mithrilDB, context));
-        MithrilDBHelper.getHelper(context).addPolicyRule(mithrilDB, DataGenerator.createPolicyRule(8,
+        MithrilDBHelper.getHelper(context).addPolicyRule(mithrilDB, DataGenerator.createPolicyRule(7,
                         "com.google.android.gm", "Gmail",
                         Manifest.permission.READ_CALENDAR,
                         MithrilAC.getPrefTimeIntervalWeekdayTemporalKey(), MithrilAC.getPrefKeyContextTypeTemporal(),
@@ -121,12 +113,12 @@ public class DataGenerator {
 
     public static void setPolicyEmailClientsWriteCalendarAccessAtWorkDuringWeekdays(SQLiteDatabase mithrilDB, Context context)
             throws SemanticInconsistencyException {
-        MithrilDBHelper.getHelper(context).addPolicyRule(mithrilDB, DataGenerator.createPolicyRule(9,
+        MithrilDBHelper.getHelper(context).addPolicyRule(mithrilDB, DataGenerator.createPolicyRule(8,
                         "com.google.android.gm", "Gmail",
                         Manifest.permission.WRITE_CALENDAR,
                         MithrilAC.getPrefWorkLocationKey(), MithrilAC.getPrefKeyContextTypeLocation(),
                         actionAllow, mithrilDB, context));
-        MithrilDBHelper.getHelper(context).addPolicyRule(mithrilDB, DataGenerator.createPolicyRule(9,
+        MithrilDBHelper.getHelper(context).addPolicyRule(mithrilDB, DataGenerator.createPolicyRule(8,
                         "com.google.android.gm", "Gmail",
                         Manifest.permission.WRITE_CALENDAR,
                         MithrilAC.getPrefTimeIntervalWeekdayTemporalKey(), MithrilAC.getPrefKeyContextTypeTemporal(),
@@ -147,6 +139,8 @@ public class DataGenerator {
         long appId, ctxtId;
         appId = MithrilDBHelper.getHelper(context).findAppIdByAppPkgName(mithrilDB, appPkgName);
         ctxtId = MithrilDBHelper.getHelper(context).findContextIdByLabelAndType(mithrilDB, contextLabel, contextType);
+        Log.d(MithrilAC.getDebugTag(), "OpCode: "+AppOpsManager.permissionToOpCode(permString));
+        Log.d(MithrilAC.getDebugTag(), "AppOps: "+AppOpsManager.opToPermission(AppOpsManager.permissionToOpCode(permString)));
         if(appId == -1 || ctxtId == -1)
             return null;
         return new PolicyRule(
