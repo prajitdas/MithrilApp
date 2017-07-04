02e2b549249d1274fd73965d06557aaa74495885
Prajit Kumar Das <prajitdas@gmail.com>
2017-07-04 05:29:31 -0400

    app changes

--- app/src/main/java/edu/umbc/ebiquity/mithril/ui/activities/FeedbackActivity.java
+++ app/src/main/java/edu/umbc/ebiquity/mithril/ui/activities/FeedbackActivity.java
@@ -5,9 +5,15 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import edu.umbc.ebiquity.mithril.MithrilAC;
 import edu.umbc.ebiquity.mithril.R;
+import edu.umbc.ebiquity.mithril.util.specialtasks.permissions.PermissionHelper;
 
 public class FeedbackActivity extends AppCompatActivity {
     private FloatingActionButton fab;
@@ -20,33 +26,30 @@ public class FeedbackActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_feedback);
         setSupportActionBar(toolbar);
 
-        fab = (FloatingActionButton) findViewById(R.id.fab_feedback);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showSnackbar(view, view.getContext().getResources().getString(R.string.functionality_not_active_yet));
-            }
-        });
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    private void showSnackbar(View view, String message) {
-        Snackbar snackbar = Snackbar.make(view,
-                message,
-                Snackbar.LENGTH_INDEFINITE);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_feedback, menu);
+        return true;
+    }
 
-        snackbar.setActionTextColor(getResources().getColor(R.color.white, this.getTheme()));
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.upload_feedback) {
+            startUpload();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-        // get snackbar view
-        View snackbarView = snackbar.getView();
-        snackbarView.setBackgroundColor(getResources().getColor(R.color.colorPrimary, this.getTheme()));
+    private void startUpload() {
 
-        snackbar.setAction(R.string.okay,
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                    }
-                }).show();
     }
 }
