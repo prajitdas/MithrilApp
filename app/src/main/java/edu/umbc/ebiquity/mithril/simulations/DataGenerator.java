76a21dd4d84118e2aeabdb52af54f113928eb0d9
Prajit Kumar Das <prajitdas@gmail.com>
2017-07-18 23:34:44 -0400

    app changes

--- app/src/main/java/edu/umbc/ebiquity/mithril/simulations/DataGenerator.java
+++ app/src/main/java/edu/umbc/ebiquity/mithril/simulations/DataGenerator.java
@@ -23,6 +23,7 @@ public class DataGenerator {
     private static Map<String, Action> permActionMap = new HashMap<>();
 
     public static void setPolicy(SQLiteDatabase mithrilDB, Context context, AppData app) throws SemanticInconsistencyException {
+        Log.d(MithrilAC.getDebugTag(), "app name:"+app.getAppName());
         permValues = MithrilDBHelper.getHelper(context).findDefaultRulesForAppCategory(mithrilDB, app.getAppCategory());
         getPoliciesForCategory();
         int policyId = MithrilDBHelper.getHelper(context).findMaxPolicyId(mithrilDB);
